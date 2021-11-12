91e7afa5c2c2c355c07d86a1ab40af90782a5ad0
dino9611 <aldinorahman36@gmail.com>
2021-11-12 11:04:51 +0700

    docker compose

--- index.js
+++ index.js
@@ -3,28 +3,34 @@ require("dotenv").config();
 const app = express();
 const mysql = require("mysql2");
 
-// const conn = mysql.createConnection({
-//   host: "host.docker.internal",
-//   port: 3306,
-//   user: "dino9611",
-//   password: "password",
-//   database: "",
-// });
+const conn = mysql.createConnection({
+  host: process.env.HOST || "localhost",
+  port: process.env.PORT || 3306,
+  user: process.env.USER ||"dino9611",
+  password: process.env.PASS ||"password",
+  database: process.env.DB|| 'bejc18',
+});
 
 
-// conn.connect((err)=>{
-//     if(err){
-//         console.log(err)
-//         return
-//     }
-//     console.log('connect thread id '+ conn.threadId )
-// })
+conn.connect((err)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log('connect thread id '+ conn.threadId )
+})
 
 app.get("/", (req, res) => {
   res.status(200).send({
     message:
-      "welcome docker nodejs",
+      "welcome docker nodejs "+process.env.SERVER,
   });
+});
+
+app.get("/user", async (req, res) => {
+
+  const [userdata] = await conn.promise().query('select * from user')
+  res.status(200).send(userdata);
 });
 
 app.listen(5000, () => console.log("app jalan di 5000 "));
