46c64c429b82b0b6ce5989a79e4ea36b236c9d77
dino9611 <45329868+dino9611@users.noreply.github.com>
2021-11-15 12:54:39 +0700

    github actions

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ require("dotenv").config();
 const app = express();
 const mysql = require("mysql2");
 
-const conn = mysql.createConnection({
-  host: process.env.HOST || "localhost",
-  port: process.env.PORT || 3306,
-  user: process.env.USER ||"dino9611",
-  password: process.env.PASS ||"password",
-  database: process.env.DB|| 'bejc18',
-});
+// const conn = mysql.createConnection({
+//   host: process.env.HOST || "localhost",
+//   port: process.env.PORT || 3306,
+//   user: process.env.USER ||"dino9611",
+//   password: process.env.PASS ||"password",
+//   database: process.env.DB|| 'bejc18',
+// });
 
 
-conn.connect((err)=>{
-    if(err){
-        console.log(err)
-        return
-    }
-    console.log('connect thread id '+ conn.threadId )
-})
+// conn.connect((err)=>{
+//     if(err){
+//         console.log(err)
+//         return
+//     }
+//     console.log('connect thread id '+ conn.threadId )
+// })
 
 app.get("/", (req, res) => {
   res.status(200).send({
     message:
-      "welcome docker nodejs "+process.env.SERVER,
+      "welcome docker nodejs dari github actions "+process.env.SERVER,
   });
 });
 
